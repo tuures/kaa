e469f9e8bbd793288ef6d5eb48470d6396ef3187
Tuure Savuoja <tuure@tuuresavuoja.net>
2017-02-17 09:42:38 +0200

    small refactor

---
+++ main.ts
@@ -0,0 +1,65 @@
+import * as R from 'ramda'
+
+import * as RegExpRuleLexer from './lib/RegExpRuleLexer'
+
+export type Token = ['SPACE', string]
+                  | ['NEWLINE', string]
+                  | ['ID', string]
+                  | ['OP', string]
+                  | ['STR', string]
+                  | ['INT', string]
+
+const lexerRules: RegExpRuleLexer.Rule<Token>[] = [
+  [
+    /[\t ]+/,
+    c => ['SPACE', c[0]]
+  ],
+  [
+    /\r?\n/,
+    c => ['NEWLINE', c[0]]
+  ],
+  [
+    /=/,
+    c => ['OP', c[0]]
+  ],
+  [
+    /'([^']+)'/,
+    c => ['STR', c[1]]
+  ],
+  [
+    /\d+/,
+    c => ['INT', c[0]]
+  ],
+  [
+    /\w+/,
+    c => ['ID', c[0]]
+  ],
+]
+
+const lexer = RegExpRuleLexer.create<Token>(lexerRules, t => t[0] === 'NEWLINE')
+
+function parser(tokens: Token[]) {
+  const tokensNoSpace = R.filter<Token>(t => t[0] !== 'SPACE')(tokens)
+}
+
+function compiler(source: string) {
+  const {tokens, error, row, col} = lexer(source)
+  if (error) {
+    console.log(`Unexpected token at ${row}:${col}`)
+  } else {
+    console.log(parser(tokens))
+  }
+}
+
+
+const src = `
+p = 'asd'
+q = 'lol'
+
+integer = 123
+
+qp = '$asd $lol $integer'
+
+`
+
+console.log(compiler(src))
